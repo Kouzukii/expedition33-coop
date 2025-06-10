ad3a6c230cdb03cb8b46d359050eedc97d9345d2
Kouzukii <ludewig.b@gmail.com>
2025-06-10 21:04:40 +0200

    Update to game version 1.3.0

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ using UAssetAPI.ExportTypes;
 using UAssetAPI.FieldTypes;
 using UAssetAPI.Kismet.Bytecode;
 using UAssetAPI.Kismet.Bytecode.Expressions;
+using UAssetAPI.PropertyTypes.Objects;
+using UAssetAPI.PropertyTypes.Structs;
 using UAssetAPI.UnrealTypes;
 using UAssetAPI.Unversioned;
 
@@ -24,6 +26,7 @@ var usmap = new Usmap("Mappings.usmap");
     Console.WriteLine("Patched ModLibrary");
 }
 
+// patch ModActor so we can set GameMode->PlayerControllerClass
 {
     var modActor = new UAsset(@".\Sandfall\Windows\Sandfall\Content\Coop\ModActor.uasset", EngineVersion.VER_UE5_4, usmap);
     var setterFunc = (FunctionExport)modActor["set_PlayerControllerClass"];
@@ -62,35 +65,73 @@ var usmap = new Usmap("Mappings.usmap");
     Console.WriteLine("Patched ModActor");
 }
 
-string[] filesToHook = [
-    "jRPGTemplate/Blueprints/Basics/BP_jRPG_Character_Battle_Base",
-    "jRPGTemplate/Blueprints/Basics/BP_jRPG_Controller_World",
-    "jRPGTemplate/Blueprints/Basics/BP_jRPG_GM_World",
-    "UI/Widgets/MainMenu/WBP_MM_MainMenu",
-    "UI/Widgets/InGame_Menu/Team/WBP_TeamCard_CharacterStatus",
-    "UI/Widgets/InGame_Menu/Settings/OptionsList/WBP_Settings_GeneralOptionsList"
-];
+// hook blueprints
+{
+    string[] filesToHook = [
+        "jRPGTemplate/Blueprints/Basics/BP_jRPG_Character_Battle_Base",
+        "jRPGTemplate/Blueprints/Basics/BP_jRPG_Controller_World",
+        "jRPGTemplate/Blueprints/Basics/BP_jRPG_GM_World",
+        "UI/Widgets/MainMenu/WBP_MM_MainMenu",
+        "UI/Widgets/InGame_Menu/Team/WBP_TeamCard_CharacterStatus",
+        "UI/Widgets/InGame_Menu/Settings/OptionsList/WBP_Settings_GeneralOptionsList"
+    ];
 
-foreach (var file in filesToHook) {
-    var proc = Process.Start(Path.Join(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "ue-hook-blueprints.exe"),
+    foreach (var file in filesToHook) {
+        var proc = Process.Start(Path.Join(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "ue-hook-blueprints.exe"),
+        [
+            $@".\Sandfall\Windows\Sandfall\Content\{file}.uasset",
+            $@".\Export\Sandfall\Content\{file}.uasset",
+            "--ueversion", "VER_UE5_4",
+            "--mappings", "Mappings.usmap"
+        ]);
+        proc.WaitForExit();
+        if (proc.ExitCode != 0)
+            throw new Exception($"ue-hook-blueprints.exe failed with {proc.ExitCode}");
+    }
+
+    var retocProc = Process.Start("retoc.exe",
     [
-        $@".\Sandfall\Windows\Sandfall\Content\{file}.uasset",
-        $@".\Export\Sandfall\Content\{file}.uasset",
-        "--ueversion", "VER_UE5_4",
-        "--mappings", "Mappings.usmap"
+        "to-zen", @".\Sandfall\Windows", "expedition33_coop_P.utoc",
+        "--version", "UE5_4",
+        "--filter", "Content/Coop",
+        ..filesToHook.SelectMany<string, string>(f => ["--filter", f])
     ]);
-    proc.WaitForExit();
-    if (proc.ExitCode != 0)
-        throw new Exception($"ue-hook-blueprints.exe failed with {proc.ExitCode}");
+    retocProc.WaitForExit();
+    if (retocProc.ExitCode != 0)
+        throw new Exception($"retoc.exe failed with {retocProc.ExitCode}");
 }
 
-var retocProc = Process.Start("retoc.exe",
-[
-    "to-zen", @".\Sandfall\Windows", "expedition33_coop_P.utoc",
-    "--version", "UE5_4",
-    "--filter", "Content/Coop",
-    ..filesToHook.SelectMany<string, string>(f => ["--filter", f])
-]);
-retocProc.WaitForExit();
-if (retocProc.ExitCode != 0)
-    throw new Exception($"retoc.exe failed with {retocProc.ExitCode}");
+// create rebinduibutton addon that changes held actions from A to X since the X button is slightly bugged in menus for players 2 and 3
+{
+    string[] actionTables = [
+        "UI/CommonUI/DataTables/DT_CommonUISpecificAction",
+        "UI/CommonUI/DataTables/DT_CommonUIActionComposite"
+    ];
+    string[] heldActions = [
+        "CommonUI_GameMenu_SkillTree_LearnSkill", "CommonUI_GameMenu_SkillTree_AssignSkill",
+        "CommonUI_GameMenu_UpgradeWeapon", "CommonUI_GameMenu_AssignAttributes"
+    ];
+    foreach (var tableAsset in actionTables) {
+        var table = new UAsset($@".\Export\Sandfall\Content\{tableAsset}.uasset", EngineVersion.VER_UE5_4, usmap);
+        var leftKeyName = new FName(table, table.SearchNameReference(FString.FromString("Gamepad_FaceButton_Left")));
+        var export = (DataTableExport)(table.Exports.Find(e => e is DataTableExport) ?? throw new Exception("Missing DataTableExport"));
+        foreach (var key in heldActions) {
+            var entry = export.Table.Data.Find(entry => entry.Name.Value.Value == key) ?? throw new Exception("Missing DataTable Entry");
+            var keyName = ((entry["DefaultGamepadInputTypeInfo"] as StructPropertyData)?["Key"] as StructPropertyData)?["KeyName"] as NamePropertyData ?? throw new Exception("Missing DefaultGamepadInputTypeInfo->Key->KeyName");
+            if (keyName.Value.Value.Value != "Gamepad_FaceButton_Bottom")
+                throw new Exception("Key is not Gamepad_FaceButton_Bottom");
+            keyName.Value = leftKeyName;
+        }
+        table.Write($@".\Sandfall\Windows\Sandfall\Content\{tableAsset}.uasset");
+    }
+
+    var retocProc = Process.Start("retoc.exe",
+    [
+        "to-zen", @".\Sandfall\Windows", "expedition33_coop_rebinduibuttons_P.utoc",
+        "--version", "UE5_4",
+        ..actionTables.SelectMany<string, string>(f => ["--filter", f])
+    ]);
+    retocProc.WaitForExit();
+    if (retocProc.ExitCode != 0)
+        throw new Exception($"retoc.exe failed with {retocProc.ExitCode}");
+}
